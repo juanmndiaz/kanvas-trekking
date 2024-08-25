9448139b46f373697356b1954dd40ad98e5bfa67
JmdZ <jmdz@JmdZs-MacBook-Pro.local>
2024-08-25 18:31:59 +0200

    chore: init project

---
+++ app/root.jsx
@@ -0,0 +1,30 @@
+import {
+    Links,
+    LiveReload,
+    Meta,
+    Outlet,
+    Scripts,
+  } from "@remix-run/react";
+  
+  export default function App() {
+    return (
+      <html>
+        <head>
+            <link
+            rel="icon"
+            href="data:image/x-icon;base64,AA"
+          />
+          <Meta />
+          <Links />
+        </head>
+        <body>
+          <LiveReload />
+          <h1>Kanvas Trekking</h1>
+          <Outlet />
+  
+          <Scripts />
+        </body>
+      </html>
+    );
+  }
+  

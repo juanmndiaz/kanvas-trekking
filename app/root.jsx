bfc9d9c119b96113da2b135ab346cef3b0a3e3bd
celiagomezdev <celiagomezdev@gmail.com>
2024-10-09 10:19:34 +0200

    feat: update config

--- app/root.jsx
+++ app/root.jsx
@@ -1,30 +1,20 @@
-import {
-    Links,
-    LiveReload,
-    Meta,
-    Outlet,
-    Scripts,
-  } from "@remix-run/react";
-  
-  export default function App() {
-    return (
-      <html>
-        <head>
-            <link
-            rel="icon"
-            href="data:image/x-icon;base64,AA"
-          />
-          <Meta />
-          <Links />
-        </head>
-        <body>
-          <LiveReload />
-          <h1>Kanvas Trekking</h1>
-          <Outlet />
-  
-          <Scripts />
-        </body>
-      </html>
-    );
-  }
-  
+import { Links, LiveReload, Meta, Outlet, Scripts } from '@remix-run/react';
+
+export default function App() {
+  return (
+    <html>
+      <head>
+        <link rel="icon" href="data:image/x-icon;base64,AA" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <LiveReload />
+        <h1>Kanvas Trekking</h1>
+        <Outlet />
+
+        <Scripts />
+      </body>
+    </html>
+  );
+}
